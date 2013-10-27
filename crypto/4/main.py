0ec0875e5abe5092c9e9bcc7befc687493a53efa
Tomas Raila <tomas.raa@gmail.com>
2013-10-27 11:32:28 +0200

    veikiantis variantas su bugu kuris reikalingas sėkmingam dešifravimui

--- crypto/4/main.py
+++ crypto/4/main.py
@@ -7,20 +7,45 @@ from itertools import chain
     
 get_bin = lambda x: x >= 0 and str(bin(x))[2:] or "-" + str(bin(x))[3:]
 
+getBin = lambda x, n: x >= 0 and str(bin(x))[2:].zfill(n) or "-" + str(bin(x))[3:].zfill(n)
+
 def pair_xor(pair1, pair2):
     return [pair1[0] ^ pair2[0], pair1[1] ^ pair2[1]]
 
+def __dec_to_bin(dec):
+    return (int)(get_bin(dec))
+
 def rotate_right(num,n):
-    l = get_bin(num)
-    return int(l[-n:] + l[:-n], 2)
+#     l = get_bin(num)
+#     return int(l[-n:] + l[:-n], 2)
+#     return str(int(l[-n:] + l[:-n], 2))
+    return num[-n:] + num[:-n]
 
 def rotate_left(num,n):
-    l = get_bin(num)
-    return int(l[n:] + l[:n], 2)
+#     l = get_bin(num)
+#     return int(l[n:] + l[:n], 2)
+#     return str(int(l[n:] + l[:n], 2))
+    return num[n:] + num[:n]
+
+def tea_it(m,k):
+    sm0=getBin(m[0], 8)
+    m1=(int(rotate_left(sm0,2))+k[1])%256
+    m2=(int(rotate_right(sm0,2))+k[2])%256 # dn=2^8=256
+    m0=(m[0]+k[0])%256
+    c=((m0^m1^m2)+m[1])%256
+    return [c,m[0]]
+
+def tea_itd(m,k):
+    sm0=getBin(m[0], 8)
+    m1=(int(rotate_left(sm0,2))+k[1])%256
+    m2=(int(rotate_right(sm0,2))+k[2])%256
+    m0=(m[0]+k[0])%256
+    c=(256-(m0^m1^m2)+m[1])%256
+    return [c,m[0]]
 
 def tea(x, key):
-    lk1 = (rotate_left(x, 2) + key[1]) % 256
-    lk2 = (rotate_right(x, 2) + key[2]) % 256
+    lk1 = (__dec_to_bin(rotate_left(x, 2) + key[1])) % 256
+    lk2 = (__dec_to_bin(rotate_right(x, 2) + key[2])) % 256
     lk3 = (x + key[0]) % 256
     l = lk1 ^ lk2 ^ lk3
     return l
@@ -28,30 +53,31 @@ def tea(x, key):
 def encrypt(pair, key):    
     left = pair[0]
     right = pair[1]
-    return [(tea(left, key) + right) % 256, left]
+    return [(tea_it(left, key) + right) % 256, left]
 
 def decrypt(pair, key):
     left = pair[0]
     right = pair[1]
-    return [right, (left - tea(right, key)) % 256]
+    return [right, (left - tea_itd(pair, key)) % 256]
 
 def encrypt_iter(pair, key):
     k0 = [key[0], key[1], key[2]]
     k1 = [key[1], key[2], key[0]]
     k2 = [key[2], key[0], key[1]]
-    p0 = encrypt(pair, k0)
-    p1 = encrypt(p0, k1)
-    p2 = encrypt(p1, k2)
-    return p2
+    p0 = tea_it(pair, k0)
+    p1 = tea_it(p0, k1)
+    p2 = tea_it(p1, k2)
+    return [p2[1], p2[0]]
     
 def decrypt_iter(pair, key):
     k0 = [key[0], key[1], key[2]]
     k1 = [key[1], key[2], key[0]]
     k2 = [key[2], key[0], key[1]]
-    p0 = decrypt(pair, k2)
-    p1 = decrypt(p0, k1)
-    p2 = decrypt(p1, k0)
-    return p2
+    p0 = tea_itd(pair, k2)
+    p1 = tea_itd(p0, k1)
+    p2 = tea_itd(p1, k0)
+#     return p2
+    return [p2[1], p2[0]]
 
 def encrypt_ecb(message, key):
     cipher = []
@@ -94,7 +120,7 @@ def decrypt_ofb(cipher, key, iv):
     message = []
     c_in = iv
     for c in cipher:
-        c_in = decrypt_iter(c_in, key)
+        c_in = encrypt_iter(c_in, key)
         message.append(pair_xor(c_in, c))
     return message
 
@@ -120,5 +146,11 @@ cbc = [[92, 48], [65, 247], [212, 16], [168, 176], [7, 138], [86, 85], [102, 136
 ofb = [[251, 24], [84, 196], [1, 232], [250, 109], [229, 88], [99, 27], [199, 52], [98, 109]]
 
 print(blocks_to_str(decrypt_ecb(ecb, key)))
+# gerai praleidote laika
+
 print(blocks_to_str(decrypt_cbc(cbc, key, iv_cbc)))
+# didelis pasisekimas
+
 print(blocks_to_str(decrypt_ofb(ofb, key, iv_ofb)))
+# margas pasaulis
+
